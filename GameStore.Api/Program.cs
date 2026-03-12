af8c72642044f50ced3f04d24dbca12b777d4418
Rriarys <rriarys@gmail.com>
2026-03-12 20:41:08 +0200

    implement /games/{id} endpoint and add CreateGameDto for game creation

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -16,4 +16,26 @@ List<GameDto> games = [
 // GET /games
 app.MapGet("/games", () => games);
 
+const string GetGameEndpointName = "GetGame";
+
+// GET /games/{id}
+app.MapGet("/games/{id}", (int id ) => games.Find(game => game.Id == id))
+    .WithName(GetGameEndpointName); // Introduce like the local constant to avoid typos and make it easier to change the endpoint name in the future.
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new (
+        games.Count + 1, // New Id
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute("GetGame", new {id = game.Id}, game); // HTTP 201 + Location header + response body. 
+});
+
 app.Run();
