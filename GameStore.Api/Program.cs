1cf3d1df1050fc4ebc84a0b875fdbc73ba2daeb7
Rriarys <rriarys@gmail.com>
2026-03-15 18:09:02 +0200

    fix: add validation attributes to CreateGameDto properties

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,11 @@
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation(); // Add validation services to the dependency injection container
+
 var app = builder.Build();
 
 app.MapGamesEndpoints(); // Map the endpoints defined in GamesEndpoints class
+
 app.Run();
