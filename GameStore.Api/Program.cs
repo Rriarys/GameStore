6abe89862dc696cccd26b2f2783f8fdfc0e07da2
Rriarys <rriarys@gmail.com>
2026-03-16 17:10:16 +0200

    add: update GameStoreContext and Program to include DbSet properties and SQLite registration

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,12 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation(); // Add validation services to the dependency injection container
+
+var connectionString = "Data Source=GmeStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connectionString); // Register the GameStoreContext with the SQLite database connection string
 
 var app = builder.Build();
 
