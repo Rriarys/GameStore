dab20759b2b7bdcbec26fb1b39c581b3e69c8d39
Rriarys <rriarys@gmail.com>
2026-03-19 19:07:25 +0200

    add: implement database seeding for game genres and update database registration in Program.cs

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,14 +1,40 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
-    public static void MigrateDb(this WebApplication app)
+    public static void MigrateDb(this WebApplication app) // Extension method to apply any pending migrations to the database
     {
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider
                              .GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder) // Extension method to add the games genres to the database if they don't already exist
+    {
+        var connectionString = "Data Source=Data/GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>
+    (
+        connectionString,
+        optionsAction: options => options.UseSeeding((context, _) =>
+        {
+            if (!context.Set<Genre>().Any())
+            {
+                context.Set<Genre>().AddRange(
+                    new Genre { Name = "Fighting" },
+                    new Genre { Name = "Action-Adventure" },
+                    new Genre { Name = "Sandbox" },
+                    new Genre { Name = "RPG" },
+                    new Genre { Name = "Puzzle-Platformer" },
+                    new Genre { Name = "Platformer" }
+                );
+
+                context.SaveChanges();
+            }
+        })
+    ); 
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -5,8 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation(); // Add validation services to the dependency injection container
 
-var connectionString = "Data Source=Data/GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connectionString); // Register the GameStoreContext with the SQLite database connection string
+builder.AddGameStoreDb(); // Seed the database with initial data if it doesn't already exist
 
 var app = builder.Build();
 
